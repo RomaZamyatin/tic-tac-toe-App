8da2d80e4820bf3c528d6c2c85747fdfb3e3616b
Roman Zamyatin <Romikz12@mail.ru>
2026-04-13 21:24:30 +0300

    вынес логику в Game

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -22,6 +22,37 @@ const Game = () => {
     setCurrentPlayer('X')
     setIsGameEnded(false)
   }
+
+  const WIN_PATTERNS = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Варианты побед по горизонтали
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Варианты побед по вертикали
+    [0, 4, 8], [2, 4, 6] // Варианты побед по диагонали
+  ];
+
+  const changeCurrentPlayer = (currentPlayer)=> {
+    return currentPlayer === 'X' ? '0' : 'X';
+  }
+
+  const onCellClick = (index)=>{
+    const newField = [...field];
+    newField[index] = currentPlayer;
+    setField(newField);
+
+    let win = WIN_PATTERNS.some(pattern =>{
+      const [i,j,k] = pattern;
+      const values = [newField[i],newField[j],newField[k]]
+      return values[0]!=='' && values.every(v => v === values[0])
+    })
+    if (win){
+      setIsGameEnded(true);
+    } else{
+      if(newField.every(cell=>cell!=='')){
+        setIsDraw(true)
+      } else{
+        setCurrentPlayer(changeCurrentPlayer(currentPlayer))  
+      }
+    }
+  }
  
 
   return (
@@ -32,13 +63,9 @@ const Game = () => {
         currentPlayer={currentPlayer}
       />
       <Field 
-        field={field} 
-        setField={setField} 
-        currentPlayer={currentPlayer}
-        setCurrentPlayer={setCurrentPlayer}
+        field={field}
+        onCellClick={onCellClick} 
         isGameEnded={isGameEnded}
-        setIsGameEnded={setIsGameEnded}
-        setIsDraw={setIsDraw}
       />
     </GameLayout>
   );
